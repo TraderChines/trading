4c884b7cc5c11a5a50d54a389ed4e321dc23dae1
Trader Chines <traderchinesvip@gmail.com>
2025-07-25 20:03:34 +0000

    Adiciona build estático para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export', // Adicionado para exportação estática
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
