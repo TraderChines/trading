9b534bc13e1eb813ce114b79358558ff170504d5
TraderChines <traderchinesvip@gmail.com>
2025-07-25 17:22:35 -0300

    Configure project for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type {NextConfig} from 'next';
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+
 const nextConfig: NextConfig = {
   output: 'export', // Adicionado para exportação estática
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -10,6 +24,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
